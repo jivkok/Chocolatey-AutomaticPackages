7da1a7d79751a49ecfbd2cae22372e4e62032f30
Jivko Kolev <jivkokgit@gmail.com>
2014-04-28 02:57:43 -0700

    Adding CloudBerryExplorer.AmazonS3

---
+++ CloudBerryExplorer.AmazonS3/Tools/ChocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+    $packageName = '{{PackageName}}'
+    $fileType = 'exe'
+    $silentArgs = '/S'
+    $uninstallerPath = Join-Path $env:ProgramW6432 'CloudBerryLab\CloudBerry Explorer for Amazon S3\uninst.exe'
+
+    if (Test-Path $uninstallerPath) {
+        Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallerPath
+    }
+
+    Write-ChocolateySuccess $packageName
+
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
