6288dfa774f58eaac7151bcaf9747153ed0aee60
Jivko Kolev <jivkokgit@gmail.com>
2015-03-21 18:45:10 -0700

    Adding XMind

---
+++ XMind/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = '{{PackageName}}'
+$fileType = 'exe'
+$silentArgs = '/verysilent'
+$url = '{{DownloadUrl}}'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url -validExitCodes @(0)
---
+++ XMind/Tools/ChocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = '{{PackageName}}'
+
+try {
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  if ($is64bit) {
+    $unpath = "${Env:ProgramFiles(x86)}\XMind\uninstall.exe"
+  } else {
+    $unpath = "${Env:ProgramFiles}\XMind\uninstall.exe"
+  }
+  Uninstall-ChocolateyPackage "$packageName" 'EXE' '/S' "$unpath" -validExitCodes @(0)
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
