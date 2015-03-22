f8473e6e9115d66a93099928ddd4ed4574e3a1b7
Jivko Kolev <jivkokgit@gmail.com>
2015-03-21 18:45:28 -0700

    TeamViewer uninstall

---
+++ teamviewer/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = '{{PackageName}}'
+$fileType = 'exe'
+$silentArgs = '/S'
+$path = 'TeamViewer'
+$file = "${path}\uninstall.exe"
+
+if (Test-Path (Join-Path ${env:ProgramFiles} $path)) {
+    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs (Join-Path ${env:ProgramFiles} $file)
+}
+if (Test-Path (Join-Path ${env:ProgramFiles(x86)} $path)) {
+    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs (Join-Path ${env:ProgramFiles(x86)} $file)
+}
+
+Write-ChocolateySuccess $packageName
