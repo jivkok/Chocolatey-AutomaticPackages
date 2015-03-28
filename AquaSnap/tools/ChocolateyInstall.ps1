17a4d8bb2812006b3d51065b7cf99990c74d1080
Jivko Kolev <jivkokgit@gmail.com>
2015-03-28 00:22:45 -0700

    Adding AquaSnap

---
+++ AquaSnap/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'msi'
+$url  = '{{DownloadUrl}}'
+$silentArgs = '/qb'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url" -validExitCodes $validExitCodes
---
+++ AquaSnap/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+$package = '{{PackageName}}'
+
+try {
+
+  # HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall
+  # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
+  # http://stackoverflow.com/questions/450027/uninstalling-an-msi-file-from-the-command-line-without-using-msiexec
+  $msiArgs = "/X{E0527016-B2F4-4EEB-97F6-A2B8C46196CA} /qb-! REBOOT=ReallySuppress"
+  Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
