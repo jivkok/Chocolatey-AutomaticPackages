0e9ab6f2805316537e95175942fbc520976c2532
Jivko Kolev <jivkokgit@gmail.com>
2015-03-28 00:54:22 -0700

    Adding 64bit for mysql.workbench

--- mysql.workbench/tools/chocolateyInstall.ps1
+++ mysql.workbench/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'msi'
-$32BitUrl  = '{{DownloadUrl}}'
+$32BitUrl = '{{DownloadUrl}}'
+$64BitUrl = $32BitUrl -replace '-win32', '-winx64'
 $silentArgs = '/passive'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
