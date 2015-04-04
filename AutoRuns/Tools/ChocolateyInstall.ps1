421b2858cc163d0d4c94a9767e9e11bf75c20e97
Jivko Kolev <jivkokgit@gmail.com>
2015-04-04 10:57:22 -0700

    Fixups

--- AutoRuns/Tools/ChocolateyInstall.ps1
+++ AutoRuns/Tools/ChocolateyInstall.ps1
@@ -1,5 +1,4 @@
 $packageName = '{{PackageName}}'
-#$url = 'http://download.sysinternals.com/files/Autoruns.zip'
 $url = '{{DownloadUrl}}'
 $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
