85ac227481fc030359f477a14a83e3fb84472441
Jivko Kolev <jivkokgit@gmail.com>
2016-06-19 16:57:58 -0700

    Merge latest changes from iozag

--- VisualStudioCode/Tools/ChocolateyInstall.ps1
+++ VisualStudioCode/Tools/ChocolateyInstall.ps1
@@ -1,6 +1,98 @@
+# Default values
+$createDesktopIcon = $true
+$createQuickLaunchIcon = $true
+$addContextMenuFiles = $true
+$addContextMenuFolders = $true
+$addToPath = $true
+
+$arguments = @{}
+$packageParameters = $env:chocolateyPackageParameters
+if ($packageParameters)
+{
+      $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+      $option_name = 'option'
+      $value_name = 'value'
+      if ($packageParameters -match $match_pattern )
+      {
+          $results = $packageParameters | Select-String $match_pattern -AllMatches
+          $results.matches | % {
+            $arguments.Add(
+                $_.Groups[$option_name].Value.Trim(),
+                $_.Groups[$value_name].Value.Trim())
+          }
+      }
+      else
+      {
+          Throw "Package Parameters were found but were invalid (REGEX Failure)"
+      }
+
+      if ($arguments.ContainsKey("nodesktopicon"))
+      {
+          Write-Host "nodesktopicon"
+          $createDesktopIcon = $false
+      }
+
+      if ($arguments.ContainsKey("noquicklaunchicon"))
+      {
+          Write-Host "noquicklaunchicon"
+          $createQuickLaunchIcon = $false
+      }
+
+      if ($arguments.ContainsKey("nocontextmenufiles"))
+      {
+          Write-Host "nocontextmenufiles"
+          $addContextMenuFiles = $false
+      }
+
+      if ($arguments.ContainsKey("nocontextmenufolders"))
+      {
+          Write-Host "nocontextmenufolders"
+          $addContextMenuFolders = $false
+      }
+
+      if ($arguments.ContainsKey("dontaddtopath"))
+      {
+          Write-Host "dontaddtopath"
+          $addToPath = $false
+      }
+}
+else
+{
+    Write-Debug "No Package Parameters Passed in"
+}
+
+$mergeTasks = "!runCode"
+if ($createDesktopIcon)
+{
+    $mergeTasks = $mergeTasks + ",desktopicon"
+}
+
+if ($createQuickLaunchIcon)
+{
+    $mergeTasks = $mergeTasks + ",quicklaunchicon"
+}
+
+if ($addContextMenuFiles)
+{
+    $mergeTasks = $mergeTasks + ",addcontextmenufiles"
+}
+
+if ($addContextMenuFolders)
+{
+    $mergeTasks = $mergeTasks + ",addcontextmenufolders"
+}
+
+if ($addToPath)
+{
+    $mergeTasks = $mergeTasks + ",addtopath"
+}
+
+Write-Host "Merge Tasks: "
+Write-Host "$mergeTasks"
+
 $packageName = '{{PackageName}}'
 $installerType = 'exe'
-$silentArgs = "/silent /mergetasks=!runcode /log=""$env:temp\vscode.log"""
+$silentArgs = "/silent /mergetasks=$mergeTasks /log=""$env:temp\vscode.log"""
 $32BitUrl  = '{{DownloadUrl}}'
 $validExitCodes = @(
     0 # success
