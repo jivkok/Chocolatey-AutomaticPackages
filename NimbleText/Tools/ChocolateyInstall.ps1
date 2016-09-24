311603ee0ed5ca5e0bedf8b4688ee73e94825959
Jivko Kolev <jivkokgit@gmail.com>
2016-09-24 11:55:50 -0700

    NimbleText as automatic package

---
+++ NimbleText/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = '{{PackageName}}'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$url64      = '{{DownloadUrlx64}}'
+
+$installerPackage = Join-Path $toolsDir "NimbleText.exe";
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileFullPath  = $installerPackage
+  url           = $url
+  url64bit      = $url64
+
+  checksum      = '{{Checksum}}'
+  checksumType  = 'sha256'
+  checksum64    = '{{Checksumx64}}'
+  checksumType64= 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
