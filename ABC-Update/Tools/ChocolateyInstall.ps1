28f2c027bd31ca91f04570503bf8b050974f4d32
Jivko Kolev <jivkokgit@gmail.com>
2016-09-25 13:18:02 -0700

    ABC-Update automatic package

---
+++ ABC-Update/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = '{{PackageName}}'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$url64      = '{{DownloadUrlx64}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+
+  checksum      = '{{Checksum}}'
+  checksumType  = 'sha256'
+  checksum64    = '{{Checksumx64}}'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'BgInfo'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
