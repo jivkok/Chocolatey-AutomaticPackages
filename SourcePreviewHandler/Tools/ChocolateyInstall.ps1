af4f9d54dccf931d62b568efd4b9ff40a3fe1f48
Jivko Kolev <jivkokgit@gmail.com>
2016-09-25 16:00:10 -0700

    SourcePreviewHandler as automatic package

--- SourcePreviewHandler/Tools/ChocolateyInstall.ps1
+++ SourcePreviewHandler/Tools/ChocolateyInstall.ps1
@@ -8,13 +8,19 @@ $url64      = '{{DownloadUrlx64}}'
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
+  fileType      = 'MSI'
   url           = $url
   url64bit      = $url64
+
+  softwareName  = '{{PackageName}}*'
 
   checksum      = '{{Checksum}}'
   checksumType  = 'sha256'
   checksum64    = '{{Checksumx64}}'
   checksumType64= 'sha256'
+
+  silentArgs    = "/passive /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
