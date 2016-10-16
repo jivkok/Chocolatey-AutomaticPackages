5348c6e152f8ef84f52eac33a2af97aca05dd268
Jivko Kolev <jivkokgit@gmail.com>
2016-10-16 09:01:57 -0700

    SqlNexus as automatic package

---
+++ SqlNexus/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = '{{PackageName}}'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$url64      = '{{DownloadUrlx64}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+
+  checksum      = '{{Checksum}}'
+  checksumType  = 'sha256'
+  checksum64    = '{{Checksumx64}}'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
