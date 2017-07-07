8d2bfeffbe3dec70378ff1b4ea9e8aac6b27f597
Jivko Kolev <jivkokgit@gmail.com>
2017-07-06 18:20:26 -0700

    Fixups - MobaXTerm, TeamViewer

--- MobaXTerm/tools/chocolateyInstall.ps1
+++ MobaXTerm/tools/chocolateyInstall.ps1
@@ -8,19 +8,25 @@ $url64      = '{{DownloadUrlx64}}'
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI'
   url           = $url
   url64bit      = $url64
-
-  softwareName  = '{{PackageName}}*'
 
   checksum      = '{{Checksum}}'
   checksumType  = 'sha256'
   checksum64    = '{{Checksumx64}}'
   checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  file          = Join-Path $toolsDir 'MobaXterm_installer.msi'
+  file64        = Join-Path $toolsDir 'MobaXterm_installer.msi'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
